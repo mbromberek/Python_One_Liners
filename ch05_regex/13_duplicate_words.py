65e434ff2dde964750c5b54b4fedeed900cb3ec8
Mike Bromberek <mikebromberek@gmail.com>
2020-04-22 19:41:02 -0500

    Finished chapter 05

---
+++ ch05_regex/13_duplicate_words.py
@@ -0,0 +1,15 @@
+## Dependencies 
+import re
+
+## Data 
+text = 'if you use words too often words become used'
+
+## One-liner
+style_problems = re.search('\s(?P<x>[a-z]+)\s+([a-z]+\s+){0,10}(?P=x)\s',' ' + text + ' ')
+
+## Results
+print(style_problems)
+# <re.Match object; span=(11, 34), match=' words too often words '>
+
+
+
